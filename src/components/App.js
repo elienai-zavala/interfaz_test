3ef7188d7351e4a56512ca30f05b5a4cd162c6c6
Elienai Zavala <elienai.zavala@macropay.mx>
2022-09-22 18:35:24 -0500

    Primer commit para inicializar el proyecto

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+//   const App = () => {
+//     return (
+//       <div>
+//         <h1> Hola Mundo! </h1>
+//       </div>
+//     );
+//   };
+
+//   export default App
+
+import Login from './Login';
+
+function App() {
+  return (
+    <main>
+      <Login />
+    </main>
+  );
+}
+
+export default App;
+  
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /\.module\.css$/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
